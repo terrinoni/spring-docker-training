96432449f4d87cddb835a4224b6df157da65e6ba
Marco Terrinoni <marco.terrinoni90@gmail.com>
2017-03-07 22:39:21 +0100

    basic structure inclusion

---
+++ src/main/java/it/terrinoni/training/springdocker/SpringDockerApplication.java
@@ -0,0 +1,12 @@
+package it.terrinoni.training.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+}
